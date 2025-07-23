b3183ad164181630b81b0f9ebd00ad00f46928c8
Ben Danzig <ben.danzig1@gmail.com>
2025-07-23 15:29:18 -0400

    .env.public created for non-sensitive keys

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
+  await dotenv.load(fileName: '.env.public');
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform, // <-- This line is critical!
   );
   
